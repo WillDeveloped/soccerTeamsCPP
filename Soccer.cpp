98ffd3aef3443480312b9e1c0bd92a30fc7a2069
William G <gilliamwlover@gmail.com>
2020-12-25 12:47:59 -0700

    Main CPP and Sample txt files
    The txt files are written such that the first number is the players number, and the second number is their rating.

---
+++ Soccer.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>      // file functions
+using namespace std;
+
+// named constant for array size goes here
+
+
+
+// structure definition goes here
+struct Player{
+   
+   
+};
+
+// begin functions
+
+
+
+
+
+int main()
+{
+   Player players[SIZE];      // array to hold player information
+
+   int playerCount = 0;       // keeps track of how many players are stored in the array
+   char menuOp;
+   int playerJersy;           // used for interactive user input
+   int playerRating;
+   
+   
+   return 0;
+}
